de3695c46e86d058f0fa810f58eb0b7efee09309
Darktel <Pisatel90@mail.ru>
2015-09-28 16:07:58 +0300

    not done

---
+++ 4_4.py
@@ -0,0 +1,11 @@
+def adder (good = 1, bad = 2, ugly = 3):
+    print(good + bad + ugly)
+
+    
+
+    
+
+if __name__ == '__main__':
+    adder ('rtyhjtr54', '56h66', '87654')
+    adder ([5,'6tgge'], [83.90], [535])
+    adder (56.00, 3.70, 7.33)
