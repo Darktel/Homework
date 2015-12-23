b121747a14068ec6f968dc7050565eafa070a9a0
Darktel <Pisatel90@mail.ru>
2015-12-23 12:42:09 +0300

    v  0.1

--- Draw text.py
+++ Draw text.py
@@ -4,8 +4,10 @@ __author__ = 'Darktel'
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QApplication, QWidget,
+    QPushButton, QToolTip, QMessageBox, QDesktopWidget)
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import QCoreApplication
 
 class Example(QWidget):
 
@@ -16,11 +18,36 @@ class Example(QWidget):
 
     def initUI(self):
 
-        self.setGeometry(300,300,300,220)
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('This is <b>MY</b> QWidget')
+
+        btn = QPushButton("Button", self)
+        btn.clicked.connect(QCoreApplication.instance().quit)
+        btn.setToolTip('This is a <U>QButton</U> Widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50,50)
+
+        self.setGeometry(300,300,2000,1000)
         self.setWindowTitle('Dark')
         self.setWindowIcon(QIcon('www.png'))
 
+        self.centr()
+
         self.show()
+
+    def centr(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', 'Are You sure to quit?', QMessageBox.Yes |
+                                     QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 if __name__ == '__main__':
 
